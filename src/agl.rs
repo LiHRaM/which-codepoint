3d0d4de38322f210d1574fe6ad57a1cd1dd3804a
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-12-28 04:27:52 +0100

    feat: basic font dump

---
+++ src/agl.rs
@@ -0,0 +1,110 @@
+use csv_macros::csv_file;
+
+type TwoField = (&'static str, &'static str);
+
+pub const GLYPH_LIST: &[TwoField] = csv_file!("maps/glyphlist.txt");
+
+pub fn map_glyph_to_string(glyph_name: &str) -> String {
+    // drop any chars after dot
+    let dropped = step1(glyph_name);
+    // split into components
+    let split: Vec<&str> = step2(dropped);
+    // map each component and concatenate the result
+    split.into_iter().map(step3).collect()
+}
+
+fn map_agl(component: &str) -> Option<String> {
+    GLYPH_LIST.iter().find_map(|&(name, code)| {
+        if component == name {
+            unicode(code).map(|c| c.to_string())
+        } else {
+            None
+        }
+    })
+}
+
+fn map_uni(component: &str) -> Option<String> {
+    if component.starts_with("uni") && component[3..].len() % 4 == 0 {
+        (3..component.len())
+            .step_by(4)
+            .map(|ix| &component[ix..ix + 4])
+            .map(unicode)
+            .collect::<Option<String>>()
+    } else if let Some(stripped) = component.strip_prefix('u') {
+        unicode(stripped).map(|c| c.to_string())
+    } else {
+        None
+    }
+}
+
+fn unicode(input: &str) -> Option<char> {
+    u32::from_str_radix(input, 16).ok().and_then(char::from_u32)
+}
+
+fn step3(component: &str) -> String {
+    if let Some(res) = map_agl(component) {
+        res
+    } else if let Some(res) = map_uni(component) {
+        res
+    } else {
+        "".into()
+    }
+}
+
+fn step2(dropped: &str) -> Vec<&str> {
+    dropped.split('\u{005F}').collect()
+}
+
+fn step1(glyph_name: &str) -> &str {
+    glyph_name.split('\u{002E}').next().unwrap_or("")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_step1() {
+        let cmp = |b, a| assert_eq!(step1(b), a);
+
+        cmp(".notdef", "");
+        cmp("test", "test");
+    }
+
+    #[test]
+    fn test_step2() {
+        let cmp = |b, a| assert_eq!(step2(b), a);
+        cmp(
+            "Lcommaaccent_uni20AC0308_u1040C",
+            vec!["Lcommaaccent", "uni20AC0308", "u1040C"],
+        );
+    }
+
+    #[test]
+    fn test_step3() {
+        let cmp = |b, a| assert_eq!(step3(b), a);
+        let cmp2 = |b, a| assert_eq!(step3(b), step3(a));
+
+        cmp("Lcommaaccent", "\u{013B}");
+        cmp("uni20AC0308", "\u{20AC}\u{0308}");
+        cmp("u1040C", "\u{1040C}");
+        cmp("uniD801DC0C", "");
+        cmp("foo", "");
+        cmp(".notdef", "");
+
+        cmp2("uni013B", "u013B");
+    }
+
+    #[test]
+    fn test_map() {
+        let cmp = |before: &str, after: &str| assert_eq!(map_glyph_to_string(before), after);
+
+        cmp(
+            "Lcommaaccent_uni20AC0308_u1040C.alternate",
+            "\u{013B}\u{20AC}\u{0308}\u{1040C}",
+        );
+
+        cmp("foo", "");
+        cmp(".notdef", "");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+mod agl;
+
+use agl::map_glyph_to_string;
+use allsorts::{
+    binary::read::ReadScope,
+    font::{read_cmap_subtable, Encoding},
+    font_data::FontData,
+    glyph_info::GlyphNames,
+    tables::{cmap::Cmap, FontTableProvider, MaxpTable},
+    tag,
+};
+use gumdrop::Options;
+use std::{convert, fmt::Debug};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[derive(Debug, Options)]
+struct Cli {
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(free, required, help = "path to font")]
+    path: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    info!("Parsing args");
+    let cli = Cli::parse_args_default_or_exit();
+
+    dump_info(cli)
+}
+
+fn dump_info(cli: Cli) -> anyhow::Result<()> {
+    info!("Load into buffer");
+    let buffer = std::fs::read(&cli.path)?;
+
+    info!("Setting ReadScope");
+    let scope = ReadScope::new(&buffer);
+
+    info!("Parse file");
+    let font_file = scope.read::<FontData>()?;
+
+    info!("Get table provider");
+    let table_provider = font_file.table_provider(0)?;
+
+    let table = table_provider
+        .table_data(tag::MAXP)?
+        .expect("no maxp table");
+    let scope = ReadScope::new(&table);
+    let maxp = scope.read::<MaxpTable>()?;
+
+    let post_data = table_provider
+        .table_data(tag::POST)
+        .ok()
+        .and_then(convert::identity)
+        .map(|data| Box::from(&*data));
+
+    let table = table_provider.table_data(tag::CMAP)?;
+    let scope = table.as_ref().map(|data| ReadScope::new(data));
+    let cmap = scope.map(|scope| scope.read::<Cmap<'_>>()).transpose()?;
+    let cmap_subtable = cmap
+        .as_ref()
+        .and_then(|cmap| read_cmap_subtable(cmap).ok())
+        .and_then(convert::identity);
+
+    if !matches!(cmap_subtable, Some((Encoding::Unicode, _))) {
+        tracing::error!("CMAP is not unicode");
+        std::process::exit(1);
+    }
+
+    let names = GlyphNames::new(&cmap_subtable, post_data);
+    for glyph_id in 0..maxp.num_glyphs {
+        let name = names.glyph_name(glyph_id);
+        println!("{}: {} - {}", glyph_id, name, map_glyph_to_string(&name));
+    }
+
+    Ok(())
+}
